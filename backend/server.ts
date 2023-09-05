fee6aa5504227e8eb76d52c3154b7cdf2843ba10
yovanha <yovanha.fajardo@salesloft.com>
2023-09-04 22:26:46 -0600

    remove comment code

--- backend/server.ts
+++ backend/server.ts
@@ -1,23 +1,22 @@
 import express, { Request, Response } from 'express';
 import pool from './db';
 
-const app = express();
-const port = process.env.PORT || 5000;
+const app = express()
+const port = process.env.PORT || 5000
 
-app.use(express.json());
+app.use(express.json())
 
 app.get('/', async (req: Request, res: Response) => {
   try {
-    // res.json({ message: 'Hello from TypeScript!' });
     const client = await pool.connect();
     const result = await client.query('SELECT * FROM test')
     const message = result.rows[0].id
     res.json({ message })
   } catch (err) {
-    console.error('Error executing SQL ', err);
+    console.error('Error executing SQL ', err)
   }
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  console.log(`Server is running on port ${port}`)
+})
