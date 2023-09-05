f9b3a950fe5aec8dad8b7f0135c66a7c664d80f9
yovanha <yovanha.fajardo@salesloft.com>
2023-09-04 22:34:28 -0600

    fix incorrect import

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api/v1': 'http://localhost:5000', // Adjust the URL to match your Express.js backend
-    },
-  },
 })
